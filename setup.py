9c50099f8ee9eea2d37c3fe978487a18f2f40ca6
Neil Hansen <neil@amber.film>
2019-01-09 17:33:44 -0500

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name='ALE2CSV',
+        version='1.0',
+        description='.ALE to .CSV converter',
+        author='Neil Hansen',
+        author_email='neil@amber.film',
+        packages=['main'],
+    )
