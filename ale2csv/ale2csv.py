8321fe38a8a111ed0b267f2143cef5c4da83daaf
Neil Hansen <neil@amber.film>
2019-01-10 18:57:09 -0500

    Added read_target() helper to allow for raw string parsing.

--- ale2csv/ale2csv.py
+++ ale2csv/ale2csv.py
@@ -1,9 +1,10 @@
-from .helpers import read_file_to_string
+from .helpers import read_target
 from .parser import parse_ale
 from .render import render_csv
 
-def ale2csv(file_path, destination, converters=None):
-    file_string = read_file_to_string(file_path)
+def ale2csv(target, destination, converters=None):
+    
+    file_string = read_target(target)
     
     ale_map = parse_ale(file_string, converters=converters)
 
--- ale2csv/helpers.py
+++ ale2csv/helpers.py
@@ -6,3 +6,11 @@ def read_file_to_string(file_path):
         raise FileNotFoundError('File does not exist.')
     with open(file_path, 'r') as f:
         return f.read()
+
+def read_target(target):
+    if not isinstance(target, str):
+        raise ValueError('ale2csv needs a string, either raw ALE or a file path.')
+    if os.path.exists(target):
+        return read_file_to_string(target)
+    else:
+        return target    
