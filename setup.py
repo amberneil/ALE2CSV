be644f7a2afa15f66e3eba9647ba1c9b51e0abc3
Neil Hansen <neil@amber.film>
2019-01-10 16:39:30 -0500

    Tweaked package_dir in setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
         version='0.1.0',
         description='.ALE to .CSV converter',
         author='Neil Hansen',
+        package_dir={'ale2csv': 'ale2csv'},
         author_email='neil@amber.film',
         packages=['ale2csv'],
     )
