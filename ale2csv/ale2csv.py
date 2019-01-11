24febefb365a9accf7965d7fb4f240ee26466582
Neil Hansen <neil@amber.film>
2019-01-10 19:23:34 -0500

    Seperated out format_csv in render.py to allow for buffer return.

--- ale2csv/ale2csv.py
+++ ale2csv/ale2csv.py
@@ -2,10 +2,12 @@ from .helpers import read_target
 from .parser import parse_ale
 from .render import render_csv
 
-def ale2csv(target, destination, converters=None):
+def ale2csv(target, destination=None, converters=None):
     
     file_string = read_target(target)
     
     ale_map = parse_ale(file_string, converters=converters)
 
-    render_csv(ale_map['data'], destination, column_labels=ale_map['columns'])
+    return render_csv(
+        ale_map['data'], destination, column_labels=ale_map['columns']
+    )
--- ale2csv/render.py
+++ ale2csv/render.py
@@ -1,13 +1,30 @@
 import csv
+import shutil
+import io
 
-def render_csv(data_list, destination, column_labels=None):
-    import os
-    print(os.path.abspath(destination))
-    with open(destination, 'w', newline='') as f:
-        if column_labels:
-            writer = csv.DictWriter(f, fieldnames=column_labels)
-            writer.writeheader()
-            for row in data_list:
-                writer.writerow(dict(zip(column_labels, row)))
-        else:
-            writer.writerows(data_list)
+def render_csv(data_list, destination=None, column_labels=None):
+    csv_buffer = format_csv(data_list, column_labels=column_labels)
+
+    if destination:
+        with open(destination, 'w') as f:
+            shutil.copyfileobj(csv_buffer, f)
+        return None
+    else:
+        return csv_buffer.getvalue()
+
+
+def format_csv(data_list, column_labels=None):
+    buff = io.StringIO()
+    if column_labels:
+        writer = csv.DictWriter(buff, fieldnames=column_labels)
+        writer.writeheader()
+        for row in data_list:
+            writer.writerow(dict(zip(column_labels, row)))
+    else:
+        writer = csv.writer(buff)
+        writer.writerows(data_list)
+    
+    buff.seek(0)
+    return buff
+
+
